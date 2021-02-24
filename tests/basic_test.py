ce0fe2cc3c6fe48851424e208189107bd975d127
Daniel Ansorregui <mailszeros@gmail.com>
2021-02-24 01:48:36 +0000

    Remove unsed test

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -8,10 +8,5 @@ class MainTest(unittest.TestCase):
         # test that 1 + 1 = 2
         self.assertEqual(fdb.add(1, 1), 2)
 
-    def test_subtract(self):
-        # test that 1 - 1 = 0
-        self.assertEqual(fdb.subtract(1, 1), 0)
-
-	
 if __name__ == '__main__':
     unittest.main()
