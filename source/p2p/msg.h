0b4f2c1a3b400153b5bdf2247182996976e2ec35
Daniel Ansorregui <mailszeros@gmail.com>
2021-03-22 21:53:34 +0000

    Finish up basic P2P
    * Chain connections and discovery
    * Check network Ids and UIDs
    * No useful data being sent, yet
    * Add printers and formatters

--- source/p2p/msg.h
+++ source/p2p/msg.h
@@ -2,13 +2,14 @@
 
 #include <msgpack.hpp>
 #include <array>
+#include <fmt/format.h>
 
 namespace Msg {
 
 enum class Type  {
     PEER_INFO,
-    WELCOME,
     DISCOVERY,
+    DISCONNECT,
 };
 
 struct Any {
@@ -17,20 +18,57 @@ struct Any {
     MSGPACK_DEFINE(type, data);
 };
 struct PeerInfo {
-    int mVersion;
-    //int mAddress;
+    // Irrelevant Misc information
     std::array<char, 16> mName;
-    MSGPACK_DEFINE(mVersion, mName);
-};
-struct Welcome {
-    PeerInfo mInfo;
-    MSGPACK_DEFINE(mInfo);
+    // General information
+    int mListenPort;
+    int mVersion;
+    int mNetID;
+    int mUID;
+    MSGPACK_DEFINE(mName, mListenPort, mVersion, mNetID, mUID);
 };
 struct Discovery {
     std::vector<std::string> mAddresses;
     MSGPACK_DEFINE(mAddresses);
 };
-
+struct Disconnect {
+    enum class Reason {
+        UNKNOWN, // Default reason, for no reason
+        WRONG_NETWORK, // Network ID missmatch
+        BLOCKING, // The peer missbehaved, blocking
+        ROTATING, // We have too many conections, closing some
+        // Add More
+    } mReason;
+    std::string mText;
+    MSGPACK_DEFINE(mReason, mText);
+};
 }; // namepsace Msg
 
 MSGPACK_ADD_ENUM(Msg::Type);
+MSGPACK_ADD_ENUM(Msg::Disconnect::Reason);
+
+// Printers
+template <>
+struct fmt::formatter<Msg::PeerInfo> {
+    constexpr auto parse(format_parse_context& ctx) { return ctx.begin(); }  template <typename FormatContext>
+    auto format(const Msg::PeerInfo& d, FormatContext& ctx) {
+        return format_to(ctx.out(), 
+            "Name:{}, Addr:{}, Version:{}, NetId:{}, UID:{}",
+             d.mName.data(), d.mListenPort, d.mVersion, d.mNetID, d.mUID);
+    }
+};
+template <>
+struct fmt::formatter<Msg::Discovery> {
+    constexpr auto parse(format_parse_context& ctx) { return ctx.begin(); }  template <typename FormatContext>
+    auto format(const Msg::Discovery& d, FormatContext& ctx) {
+        return format_to(ctx.out(), "'{}'", fmt::join(d.mAddresses, ", "));
+    }
+};
+template <>
+struct fmt::formatter<Msg::Disconnect> {
+    constexpr auto parse(format_parse_context& ctx) { return ctx.begin(); }  template <typename FormatContext>
+    auto format(const Msg::Disconnect& d, FormatContext& ctx) {
+        return format_to(ctx.out(), "{}:{}", 
+            magic_enum::enum_name(d.mReason), d.mText);
+    }
+};
